aee333502353c3a5fdfc3855e19c390472f7046a
wis-ngops <109586766+wis-ngops@users.noreply.github.com>
2022-07-19 16:50:00 +0700

    Add files via upload

---
+++ styling2.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+  margin:5,
+  loop:true,
+  autoWidth:true,
+  nav:true,
+  autoplay:true,
+  autoplayTimeout:4000,
+})
